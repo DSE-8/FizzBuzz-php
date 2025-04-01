936c04ed32429a822846515df6fb267015c45782
Diego Suarez <suarez.153960@e.unavarra.es>
2025-04-01 13:16:49 +0200

    [verde] - añaden 2 panes a la lista y devuelve la lista con 2 panes

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -4,13 +4,19 @@ namespace Deg540\CleanCodeKata9;
 
 use PHPUnit\Framework\Constraint\IsEqual;
 use function PHPUnit\Framework\equalTo;
+use function PHPUnit\Framework\isNull;
 
 class FizzBuzz
 {
     public function getList(String $action){
-        if(strtolower($action)=="añadir pan"){
-            return "pan x1";
+        $accion = strtolower($action);
+        $accionArray = preg_split("/[ ]/",$accion);
+        if($accionArray[0] != "añadir"){
+            return '';
         } 
-        return '';
+        if(2 == count($accionArray)){
+            return 'pan x1';
+        } 
+        return "pan x".$accionArray[2];
     } 
 }
