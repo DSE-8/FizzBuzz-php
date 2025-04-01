8dd731b81c98cbeb5412dcd9128468c822d05048
Diego Suarez <suarez.153960@e.unavarra.es>
2025-04-01 12:50:45 +0200

    [verde] - no doy valor y me devuelve una lista vacia

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -7,42 +7,7 @@ use function PHPUnit\Framework\equalTo;
 
 class FizzBuzz
 {
-    private const FIZZ_NUMBER = 3;
-    private const BUZZ_NUMBER = 5;
-    public function convert(int $value){
-        if($this->isFizzBuzz($value)){
-            return 'FizzBuzz';
-        } 
-        if($this->isBuzz($value)){
-            return 'Buzz';
-        } 
-        if($this->isFizz($value)){ 
-            return 'Fizz';
-        } 
-        return $value;
-    } 
-
-    private function isFizzBuzz($value): bool{
-        return ($this->isFizz($value) && $this->isBuzz($value));
-    } 
-
-    private function isFizz($value): bool{
-        return ($this->contains($value,self::FIZZ_NUMBER) || $value%self::FIZZ_NUMBER==0);
-    } 
-
-    private function isBuzz($value): bool{
-        return ($this->contains($value,self::BUZZ_NUMBER) || $value%self::BUZZ_NUMBER==0);
-    } 
-
-    private function contains(int $number,int $contained): bool{
-        $container = $number;
-        while($container>0){
-            if($container%10 == $contained){
-                return True;
-            }
-            $container = ($container - ($container%10))/10;
-        } 
-        return False;
-        
+    public function getList(){
+        return '';
     } 
 }
--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -10,65 +10,14 @@ use function PHPUnit\Framework\assertEquals;
 
 final class FizzBuzzTest extends TestCase
 {
-    private FizzBuzz $fizzBuzz;
-    protected function setUp(): void{
-        parent::setup();
-        $this->fizzBuzz = new FizzBuzz();
-    } 
     /**
      * @test
      */
-    public function notMultipleOfBuzzNumberOrFizzNumberOrContainsThemReturnsItself(){
-        $convertedValue = $this->fizzBuzz->convert(1);
+    public function givenNothingReturnsEmptyString(){
+        $ListaDeLaCompra = new FizzBuzz();
 
-        assertEquals(1,$convertedValue);
-    }
-    /**
-     * @test
-     */
-    public function multipleOfFizzNumberReturnsFizz(){
-        $convertedValue = $this->fizzBuzz->convert(6);
+        $convertedValue = $ListaDeLaCompra->getList();
 
-        assertEquals('Fizz',$convertedValue);
-    }
-    /**
-     * @test
-     */
-    public function numberThatContainsFizzNumberReturnsFizz(){
-        $convertedValue = $this->fizzBuzz->convert(13);
-
-        assertEquals('Fizz',$convertedValue);
-    }
-    /**
-     * @test
-     */
-    public function multipleOfBuzzNumberReturnsBuzz(){
-        $convertedValue = $this->fizzBuzz->convert(10);
-
-        assertEquals('Buzz',$convertedValue);
-    }
-    /**
-     * @test
-     */
-    public function numberThatContainsBuzzNumberReturnsBuzz(){
-        $convertedValue = $this->fizzBuzz->convert(52);
-
-        assertEquals('Buzz',$convertedValue);
-    }
-    /**
-     * @test
-     */
-    public function multipleOfBuzzNumberAndFizzNumberReturnsFizzBuzz(){
-        $convertedValue = $this->fizzBuzz->convert(60);
-
-        assertEquals('FizzBuzz',$convertedValue);
-    }
-    /**
-     * @test
-     */
-    public function numberThatContainsBuzzNumberAndFizzNumberReturnsFizzBuzz(){
-        $convertedValue = $this->fizzBuzz->convert(53);
-
-        assertEquals('FizzBuzz',$convertedValue);
+        assertEquals('',$convertedValue);
     }
 }
